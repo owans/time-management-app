b6967be33be26c9a1f89f11f225b6d723f929f93
Owanate Amachree <amachreeowanate@gmail.com>
2019-04-05 04:42:50 -0400

    edited package.json

--- timeoffapp/server.js
+++ timeoffapp/server.js
@@ -1,18 +1,17 @@
+
 const express = require('express');
 const path = require('path');
 const externalRequest = require('./middleware');
-const port = process.env.PORT || 3035;
+const port = process.env.PORT || 3032;
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
 app.use('/', express.static(path.join(__dirname, './build')));
 
-app.use('/request', (req, res) => {
-    res.sendFile(path.join(__dirname, './build/index.html'));
-});
+app.get('/request', (req, res) => {
+        res.sendFile(path.join(__dirname, './build/index.html'));
+    });
+
 
 app.get('/holidays', externalRequest);
 
