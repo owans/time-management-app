79c776123d6d10c0c8194a45f1a80e5423ed6354
Owanate Amachree <amachreeowanate@gmail.com>
2019-04-30 04:02:38 +0100

    made changes

---
+++ timeoffapp/build/precache-manifest.8132e0964bf053204ac4afd9f1643296.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cfc91e7ee74d1d522df",
+    "url": "/static/css/main.0873f37b.chunk.css"
+  },
+  {
+    "revision": "9cfc91e7ee74d1d522df",
+    "url": "/static/js/main.77e05952.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7a5ba9714bb62ed4d0e9",
+    "url": "/static/css/2.46eb1a4f.chunk.css"
+  },
+  {
+    "revision": "7a5ba9714bb62ed4d0e9",
+    "url": "/static/js/2.a3f01dd8.chunk.js"
+  },
+  {
+    "revision": "52145c5d17f509927c3d6dfc44e04d86",
+    "url": "/static/media/manage.52145c5d.svg"
+  },
+  {
+    "revision": "6a3966fb7a834f454712c4596dd67d99",
+    "url": "/static/media/profile.6a3966fb.svg"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/static/media/pagenotfound.6695af99.svg"
+  },
+  {
+    "revision": "16976929f22da7c089d5cf2ecbcf519f",
+    "url": "/index.html"
+  }
+];
