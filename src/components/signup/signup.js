34a22b47b857502dd6cb2e72a1d2ae2886a446bf
Owanate Amachree <amachreeowanate@gmail.com>
2019-05-02 19:39:17 +0100

    fixed header for signup form

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignUpForm from '../signup/signupform';
+import MainHeader from "../common/mainheader";
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default class SignUp extends React.Component{
   render(){
     return(
       <div>
+        <MainHeader/>
         <SignUpForm/>
       </div>
     )
