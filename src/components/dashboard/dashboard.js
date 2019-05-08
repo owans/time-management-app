57dbcf009b5b58aa3135501172f35382ff549110
Owanate Amachree <amachreeowanate@gmail.com>
2019-05-08 07:06:34 +0100

    made changes to dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -9,41 +9,46 @@ import Footer from '../footer/footer';
 import env from "../../env";
 import axios from 'axios';
 
-const typeOfTimeOff = [
+const leaveType = [
   {name: 'Vacation', days: 2},
   {name: 'Maternity Leave', days: 10},
-  {name: 'Medical', days: 5},
-  {name: 'Marriage', days: 7},
 ]
-
-
-const requests = [
-    {name: 'Sick Leave', date: '12/03/2019'},
-    {name: 'Maternity Leave', date: '23/02/2019'},
-    {name: 'Study Leave', date: '14/01/2019'},
-]
-
-const calendarDate = [
-  new Date(2019 , 0, 9), new Date(2019, 1, 23), new Date(2018, 2, 25), new Date(2019, 3, 11)
-]
-const MoreCalendarDate = [
-  new Date(2019 , 0, 9), new Date(2019, 1, 23), new Date(2018, 2, 25), new Date(2019, 3, 11),
-  new Date(2019 , 4, 2), new Date(2019, 5, 13), new Date(2018, 6, 25), new Date(2019, 7, 21),
-  new Date(2019 , 8, 16), new Date(2019, 9, 8), new Date(2018, 10, 30), new Date(2019, 11, 24)
-]
-
 class Dashboard extends React.Component{
   constructor(props){
     super(props);
 
     this.state = {
       loading: true,
-      showMore: false,
-      showMoreText: 'Show More',
-      user: ""
+      user: "",
+      userInfo: "",
+      allLeaveRequest: "",
+      availableLeaveRequest: "",
+      slicedRequest: ""
   }
   
 }
+
+convertDate = date => {
+    return new Date(date).toLocaleDateString();
+  };
+
+  getStatusColor(status) {
+    switch (status) {
+      case "declined":
+        return "badge-danger";
+      case "approved":
+        return "badge-success";
+      default:
+        return "badge-warning";
+    }
+  }
+
+getAvailableRequest(requests) {
+    if (requests) {
+      const leave = requests.filter(item => item.status === "pending");
+      this.setState({ availableLeaveRequest: leave });
+    }
+  }
 
 async componentDidMount(){
     try{
@@ -57,8 +62,22 @@ async componentDidMount(){
             }
         })
 
-        this.setState({loading:false, user: res.data.data});
+        const leave = await axios.get(`${env.api}/request`, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+
+          const request = leave.data.data;
+        const sliced = request.slice(-1);
+
+        this.setState({loading:false, 
+            user: res.data.data, 
+            allLeaveRequest: request,
+            slicedRequest: sliced
+        });
         console.log(this.state.user);
+        console.log(this.state.allLeaveRequest);
         
     }catch(err){
       if (localStorage.getItem("owatimer-token")) {
@@ -68,16 +87,13 @@ async componentDidMount(){
     }
 }
 
-
-handeleShowMore = () => {
-    this.setState({
-        showMore: !this.state.showMore,
-        showMoreText: 'Show Less'
-    })
-}
-
 render(){
-    if (this.state.loading) return <p>loading...</p>
+    if (this.state.loading)
+    return (
+      <p>
+        loading <i className="fa fa-spinner" />{" "}
+      </p>
+    );
     return(
         <div>
             <EmployeeHeader/>
@@ -98,127 +114,135 @@ render(){
                 </div>
                 </div>
 
-                <div className="col-md-3 sta">
-                <div className="card">
-                <div className="card-header text-center bg-secondary text-light">Used so far</div>
+                <div className="col-sm-10 col-md-6">
+              <div className="card">
+                <div className="card-header bg-secondary text-light">
+                  Request history
+                </div>
                 <div className="card-body">
+                  {this.state.slicedRequest.length > 0 ? (
+                    <ul className="list-group">
+                      {this.state.slicedRequest.map((item, index) => {
+                        return (
+                          <div key={index}>
+                            <li className="list-group-item">
+                              {item.leaveType}
+                              <span className="badge badge-primary float-right ">
+                                {this.convertDate(item.startDate)}
+                              </span>
+                            </li>
+                          </div>
+                        );
+                      })}
+                    </ul>
+                  ) : (
+                    <h6 className="mt-4 pb-4 text-center">
+                      No Pending leave Request
+                    </h6>
+                  )}
+                </div>
+                </div>
+                </div>
+
+                <div className="col-md-4 sta">
+                    <div className="card">
+                    <div className="card-header bg-secondary text-center text-light">Available Types</div>
+                    <div className="card-body">
                     <ul className="list-group"> 
-                        {
-                            typeOfTimeOff.map((item , index) => {
-                                return <div key={index}>
-                                    <li  className="list-group-item"> {item.name} 
-                                        <span className="badge badge-primary float-right ">{item.days}</span></li>
-                                    </div>
+                        {leaveType.map(item => {
+                            return <div key={item.date}>
+                                <li className="list-group-item"> {item.name} 
+                                <span className="badge badge-primary float-right ">{item.date}</span>
+                                </li>
+                                </div>
                                 })
                             }
-                        </ul>
+                    </ul>
+                    </div> 
+                    </div>
+                    </div>
+
+                    <div className="col-md-3 sta">
+                        <div className="card">
+                        <div className="card-header text-center bg-secondary text-light">Details</div>
+                        <div className="card-body text-center">
+                            <i className="fa fa-user-circle-o fa-3x "></i>
+                        <div className="mt-2">
+                            <h6>Name: {this.state.user.firstName}</h6>
+                            <h6>Department: {this.state.user.manager}</h6>
+                            <h6>Position: {this.state.user.department}</h6>
+                        </div>
                         </div> 
                         </div>
                         </div>
-
-
-                        <div className="col-md-4 sta">
-                        <div className="card">
-                            <div className="card-header bg-secondary text-center text-light">Available Types</div>
-                            <div className="card-body">
-                            <ul className="list-group"> 
-                                {
-                                requests.map(item => {
-                                        return <div key={item.date}>
-                                                <li className="list-group-item"> {item.name} 
-                                                    <span className="badge badge-primary float-right ">{item.date}</span>
-                                                </li>
-                                            </div>
-                                        })
-                                }
-                            </ul>
-                            </div> 
-                        </div>
-                        </div>
-
-                        <div className="col-md-3 sta">
-                        <div className="card">
-                            <div className="card-header text-center bg-secondary text-light">Details</div>
-                            <div className="card-body text-center">
-                            <i className="fa fa-user-circle-o fa-3x "></i>
-                                <div className="mt-2">
-                                    <h6>Name: {this.state.user.firstName}</h6>
-                                    <h6>Department: {this.state.user.manager}</h6>
-                                    <h6>Position: {this.state.user.department}</h6>
-                                </div>
-                                </div> 
-                                </div>
-                        </div>
                       </div>
                     </div>
-                <h3 className="container text-secondary mt-4 mb-3 ">
-                    Calendar  <button 
-                    onClick={this.handeleShowMore} 
-                    className="btn btn-secondary">{
-                        !this.state.showMore ? 'Show More' : 'Show Less'
-                    }</button> </h3>
-                    <div className="container row m-auto">
-                    { !this.state.showMore ?
-                        calendarDate.map((item, index) => {
-                            return <div key={index} className="col-md-3">
-                            <Calendar 
-                                value={item}
+                    <div className="container">
+                        <h2 className="text-center mt-4 mb-3">Calendar</h2>
+                    <div className="row align-content-center">
+                        {this.state.allLeaveRequest.length > 0 ? (
+                        this.state.allLeaveRequest.map((item, index) => {
+                        return (
+                            <div key={index} className="col-md-3 ">
+                                <Calendar
+                                value={[
+                                new Date(item.startdate),
+                                new Date(item.stopdate)
+                                ]}
                                 />
-                            </div>  
-                        }) : MoreCalendarDate.map((item, index) => {
-                            return <div key={index} className="col-md-3 mb-2">
-                            <Calendar
-                                value={item}
-                                />
-                            </div>  
-                        })
-                    }
-                    </div>   
+                            </div>
+                        );
+                    })
+                ) : (
+                        <h6>No Active Calender Pl Fill A New Absence</h6>
+                    )}
+                </div>
+                </div>   
                 <div>
-            <div className="d-flex justify-content-between ml-3 mt-5">
+                <div className="d-flex justify-content-between ml-3 mt-5">
                 <h3 className="head text-secondary container">All Absences</h3>
-            </div>
+                </div>
 
-            <div className="d-flex justify-content-center dashboard-absences mx-3">
-            <table class="table container responsive table-sm">
-            <thead>
-                <tr>
-                    <th scope="col">Types</th>
-                    <th scope="col">Duration</th>
-                    <th scope="col">Dates</th>
-                    <th scope="col">Status</th>
-                    <th scope="col"></th>
-                    <th scope="col">Approved By</th>
-                </tr>
-            </thead>
-                <tbody className="">
-                    <tr>
+                <div className="d-flex justify-content-center dashboard-absences mx-3">
+                    <table class="table container responsive table-sm">
+                        <thead>
+                            <tr>
+                            <th scope="col">Types</th>
+                            <th scope="col">Duration</th>
+                            <th scope="col">Dates</th>
+                            <th scope="col">Status</th>
+                            <th scope="col"></th>
+                            <th scope="col">Approved By</th>
+                            </tr>
+                        </thead>
+                        <tbody className="">
+                        <tr>
                         <td>Study leave</td>
                         <td>10</td>
                         <td>2021-1-15 - 2021-1-25</td>
                         <td>Approved</td>
                         <td><button><i class="fas fa-trash-alt delete"></i></button></td>
                         <td>Kunle</td>
-                    </tr>
+                        </tr>
 
-                    <tr>
+                        <tr>
                         <td>Health</td>
                         <td>5</td>
                         <td>2019-3-22 - 2019-3-27</td>
                         <td>Pending</td>
                         <td><button><i class="fas fa-trash delete"></i></button></td>
                         <td>Mayowa</td>
-                    </tr>
+                        </tr>
 
-                    <tr>
+                        <tr>
                         <td>Holiday</td>
                         <td>10</td>
                         <td>2019-4-5 - 2019-4-15</td>
                         <td>Pending</td>
                         <td><button><i class="fas fa-trash delete"></i></button></td>
                         <td>Kunle</td>
-                    </tr>
-                    <tr>
+                        </tr>
+                        <tr>
       
                         <td>Time Off</td>
                         <td>14</td>
@@ -226,9 +250,9 @@ render(){
                         <td>Pending</td>
                         <td><button><i class="fas fa-trash delete"></i></button></td>
                         <td>Mayowa</td>
-                    </tr>
-                </tbody>
-                </table>
+                        </tr>
+                    </tbody>
+                    </table>
                 </div>
             </div>
  
